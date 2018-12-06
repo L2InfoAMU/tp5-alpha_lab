96e908ecd803164ed9217b300f8e3e3246f6f157
per0xyde <jad.mouallem@etu.univ-amu.fr>
2018-12-06 20:00:31 +0100

    Classe PaletteRasterImage : ajout methode createRepresentation

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -2,14 +2,25 @@ package image;
 
 import javafx.scene.paint.Color;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class PaletteRasterImage implements Image {
+public class PaletteRasterImage  {
 
     private List<Color> palette;
-    private Color[][] pixels;
+    private int[][] indexesOfColors;
     private int width;
     private int height;
+
+
+
+
+    public void createRepresentation(){
+        palette = new ArrayList<Color>();
+        indexesOfColors = new int[width][height];
+    }
+
+
 
 
 }
