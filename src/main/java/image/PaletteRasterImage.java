2c42319fc10d9d0e25a4d03757cbad432f449e76
per0xyde <jad.mouallem@etu.univ-amu.fr>
2018-12-06 19:21:03 +0100

    Classe PaletteRasterImage : ajout des attributs

---
+++ src/main/java/image/PaletteRasterImage.java
@@ -0,0 +1,15 @@
+package image;
+
+import javafx.scene.paint.Color;
+
+import java.util.List;
+
+public class PaletteRasterImage implements Image {
+
+    private List<Color> palette;
+    private Color[][] pixels;
+    private int width;
+    private int height;
+
+
+}
