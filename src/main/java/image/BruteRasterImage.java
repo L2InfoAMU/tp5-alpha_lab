35da4f71f396b777278ee91c0782399a8d2b6230
per0xyde <jad.mouallem@etu.univ-amu.fr>
2018-12-07 21:36:28 +0100

    Classe BruteRasterImage : ajout exceptions

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -11,15 +11,30 @@ public class BruteRasterImage implements Image{
 
 
     public BruteRasterImage(Color color, int width, int height){
+
+        if(width < 0)
+            throw new IllegalArgumentException("width cannot be less than 0");
+        if(height < 0)
+            throw new IllegalArgumentException("height cannot be less than 0");
+        if (color == null)
+            throw new NullPointerException("Color cannot be null");
+
         setHeight(height);
         setWidth(width);
+
         createRepresentation();
         setPixelsColor(color);
     }
 
     public BruteRasterImage(Color[][] pixels){
+
+        Matrices.requiresNonNull(pixels);
+        Matrices.requiresNonZeroDimensions(pixels);
+        Matrices.requiresRectangularMatrix(pixels);
+
         setHeight(pixels[0].length);
         setWidth(pixels.length);
+
         createRepresentation();
         setPixelsColor(pixels);
     }
@@ -28,24 +43,25 @@ public class BruteRasterImage implements Image{
         pixels = new Color[width][height];
     }
 
+    public Color getPixelColor(int x, int y){
+        if(x<0 || y<0 || x>=pixels.length || y>=pixels[0].length)
+            throw new IllegalArgumentException("x or y cannot be outside the array bonds");
+        return pixels[x][y];
+    }
+
     public void setPixelColor(Color color, int x, int y){
         if (color == null)
             throw new NullPointerException("Color cannot be null");
-        if(x<0 || y<0 || x>=pixels.length || y>=pixels.length)
+        if(x<0 || y<0 || x>=pixels.length || y>=pixels[0].length)
             throw new IllegalArgumentException("x or y cannot be outside the array bonds");
         pixels[x][y] = color;
-    }
-
-    public Color getPixelColor(int x, int y){
-        if(x<0 || y<0 || x>=pixels.length || y>=pixels.length)
-            throw new IllegalArgumentException("x or y cannot be outside the array bonds");
-        return pixels[x][y];
     }
 
     private void setPixelsColor(Color[][] pixels){
         Matrices.requiresNonNull(pixels);
         Matrices.requiresNonZeroDimensions(pixels);
-        this.pixels = pixels;
+        Matrices.requiresRectangularMatrix(pixels);
+        this.pixels = pixels.clone();
     }
 
     private void setPixelsColor(Color color){
@@ -75,7 +91,7 @@ public class BruteRasterImage implements Image{
 
     protected void setHeight(int height){
         if(height < 0)
-            throw new IllegalArgumentException("width cannot be less than 0");
+            throw new IllegalArgumentException("Height cannot be less than 0");
         this.height = height;
     }
 
