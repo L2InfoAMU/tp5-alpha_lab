c56a97b180a40d48abacf37fe019e501da221a66
per0xyde <jad.mouallem@etu.univ-amu.fr>
2018-12-07 20:03:09 +0100

    Classe BruteRasterImage : ajout des exceptions

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -1,6 +1,7 @@
 package image;
 
 import javafx.scene.paint.Color;
+import util.Matrices;
 
 public class BruteRasterImage implements Image{
 
@@ -28,18 +29,29 @@ public class BruteRasterImage implements Image{
     }
 
     public void setPixelColor(Color color, int x, int y){
+        if (color == null)
+            throw new NullPointerException("Color cannot be null");
+        if(x<0 || y<0 || x>=pixels.length || y>=pixels.length)
+            throw new IllegalArgumentException("x or y cannot be outside the array bonds");
         pixels[x][y] = color;
     }
 
     public Color getPixelColor(int x, int y){
+        if(x<0 || y<0 || x>=pixels.length || y>=pixels.length)
+            throw new IllegalArgumentException("x or y cannot be outside the array bonds");
         return pixels[x][y];
     }
 
     private void setPixelsColor(Color[][] pixels){
+        Matrices.requiresNonNull(pixels);
+        Matrices.requiresNonZeroDimensions(pixels);
         this.pixels = pixels;
     }
 
     private void setPixelsColor(Color color){
+        if (color == null)
+            throw new NullPointerException("Color cannot be null");
+
         for(int i=0; i<width;i++){
             for(int j=0; j<height;j++){
                 setPixelColor(color,i,j);
@@ -56,10 +68,14 @@ public class BruteRasterImage implements Image{
     }
 
     protected void setWidth(int width){
+        if(width < 0)
+            throw new IllegalArgumentException("width cannot be less than 0");
         this.width = width;
     }
 
     protected void setHeight(int height){
+        if(height < 0)
+            throw new IllegalArgumentException("width cannot be less than 0");
         this.height = height;
     }
 
