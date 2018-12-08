828ae96ce49670b15b3d97001e12bb7c4d4f4c27
per0xyde <jad.mouallem@etu.univ-amu.fr>
2018-12-08 20:29:05 +0100

    Classe SparseRasterImage : ajout getter/setters

--- src/main/java/image/SparseRasterImage.java
+++ src/main/java/image/SparseRasterImage.java
@@ -1,6 +1,7 @@
 package image;
 
 import javafx.scene.paint.Color;
+import util.Matrices;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +18,59 @@ public class SparseRasterImage extends RasterImage {
         super(pixels);
     }
 
+    public void createrepresentation()
+    {
+        pixelsMap = new HashMap<>();
+    }
 
+    private void requiresPresenceOfPoint(Point point){
+        if(!(pixelsMap.containsKey(point)))
+            throw new IllegalArgumentException(("Cannot find pixel"));
+    }
+
+    public Color getPixelColor(int x, int y)
+    {
+        requiresValidCoordinates(x, y);
+        Point point = new Point(x,y);
+        requiresPresenceOfPoint(point);
+        return pixelsMap.get(point);
+    }
+
+    public void setPixelColor(Color color, int x, int y)
+    {
+        requiresNonNullColor(color);
+        requiresValidCoordinates(x,y);
+        Point point = new Point(x,y);
+        pixelsMap.put(point,color);
+    }
+
+    public void setPixelsColor(Color [][] pixels) {
+
+        Matrices.requiresNonNull(pixels);
+        Matrices.requiresNonZeroDimensions(pixels);
+        Matrices.requiresRectangularMatrix(pixels);
+
+        pixelsMap.clear();
+
+        for(int i=0; i<this.getWidth(); i++){
+            for (int j=0; j<this.getHeight();j++) {
+                Color currentColor = pixels[i][j];
+                setPixelColor(currentColor,i,j);
+            }
+        }
+    }
+
+    public void setPixelsColor(Color color) {
+        requiresNonNullColor(color);
+
+        pixelsMap.clear();
+
+        for(int i=0; i<this.getWidth();i++) {
+            for(int j=0; j<this.getHeight();j++) {
+                setPixelColor(color,i,j);
+            }
+        }
+    }
 
 
 
